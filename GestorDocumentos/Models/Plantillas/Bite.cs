416ee62862e9eeb89d347ec033dd404939b13e8b
Patrick Figueroa <pat.figueroaf@alumnosduoccl.onmicrosoft.com>
2020-03-06 21:06:59 -0300

    Plantilla BITE y paginación

---
+++ GestorDocumentos/Models/Plantillas/Bite.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GestorDocumentos.Models.Plantillas
+{
+    public class Bite
+    {
+        public string articulo { get; set; }
+        public string codBuscar { get; set; }
+        public string numero { get; set; }
+        public string alltext { get; set; }
+        public string com { get; set; }
+        public string codt { get; set; }
+        public string jj { get; set; }
+        public string ej { get; set; }
+        public string circ { get; set; }
+        public string ofi { get; set; }
+        public string res { get; set; }
+        public string lyd { get; set; }
+        public string FechaD { get; set; }
+        public string FechaH { get; set; }
+        public string pagina { get; set; }
+    }
+}
